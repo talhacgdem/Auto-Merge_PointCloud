1d6a2c4a12365e7060f4dc8bc3b0fc97ab92eca0
Muhammed Talha ÇİĞDEM <talhacgdem@hotmail.com>
2022-09-07 17:52:58 +0300

    read,distance

--- distance.py
+++ distance.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def get_distance(p, q):
     """
     Return euclidean distance between points p and q
@@ -12,11 +15,26 @@ def get_distance(p, q):
     distance = s_sq_difference ** 0.5
     return distance
 
-# 0.8055060521187908
-# check the function
-a = (1.62809, 1.43013, 1.75379)
-b = (2.04509, 1.63013, 1.62879)
-# distance b/w a and b
-d = get_distance(a, b)
-# display the result
-print(d)
+
+def findClosest(arr):
+    min_val = sys.maxsize
+    first = (arr[0][0], arr[0][1], arr[0][2])
+    a = 1
+    while a < len(arr):
+        second = (arr[a][0], arr[a][1], arr[a][2])
+        distance = get_distance(first, second)
+        if distance < min_val:
+            min_val = distance
+            print("Closest point found on index : {} , Distance is : {}".format(a, distance))
+        # else:
+            # print("     ---> On index : {}".format(a))
+
+        a += 1
+# # 0.8055060521187908
+# # check the function
+# a = (1.62809, 1.43013, 1.75379)
+# b = (2.04509, 1.63013, 1.62879)
+# # distance b/w a and b
+# d = get_distance(a, b)
+# # display the result
+# print(d)
--- readfile.py
+++ readfile.py
@@ -1,3 +1,6 @@
+import visualize
+import distance
+
 path = "files/pts2.pts"
 
 ptsFile = open(path, 'r')
@@ -6,25 +9,29 @@ lines = ptsFile.readlines()
 
 def getCoordinates(line):
     linearr = str(line).split(' ')
-    arr = {"x": linearr[0], "y": linearr[1], "z": linearr[2]}
+    # arr = {"x": linearr[0], "y": linearr[1], "z": linearr[2]}
+    arr = [float(linearr[0]), float(linearr[1]), float(linearr[2])]
     return arr
 
 
-lineNumber = 0
+lineNumber = 1
 coordinates = []
-# Strips the newline character
+
+num_rows = int(lines[0])
+lines.pop(0)
+
 for line in lines:
-    if lineNumber > 100:
+    if lineNumber > 1000000:
         break
 
-    if lineNumber == 0:
-        print("Toplam nokta sayısı : {:,}".format(int(line.strip())))
-    else:
-        coordinates.append(getCoordinates(line))
-
+    coordinates.append(getCoordinates(line))
+    print("On line : {} / {}".format(lineNumber, num_rows))
     lineNumber += 1
 
 
-import matplotlib.pyplot as plt
+distance.findClosest(coordinates)
 
-from mpl_toolkits.mplot3d import Axes3D
+# print("##################################")
+# print("Visualing...")
+# print("##################################")
+# visualize.toVisual(coordinates)
